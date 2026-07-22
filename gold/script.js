c60b7e628c090f6599b68b9031ae5c1a192d1f64
aoki-jh-school <aoki-jhs@wataridori.co.jp>
2026-07-22 11:19:38 +0900

    sukoahennko

--- gold/script.js
+++ gold/script.js
@@ -105,7 +105,12 @@ document.addEventListener("keydown", (e) => {
     renderWord();
     if (position === currentWord.length) {
       // 1単語すべて打てた
-      score += 10;
+     if (timeLeft <= 15) {
+score += 20;
+} else {
+score += 10;
+}
+
       labelEl.textContent = "正解";
       scoreEl.textContent = score;
       nextWord();
